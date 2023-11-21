7fd2eb38edb7a1f7721ded03f04054fa65593442
Dr00pSn00t <49376124+Dr00pSn00t@users.noreply.github.com>
2023-11-21 14:33:20 -0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -2,13 +2,18 @@
 
 int main()
 {
-    const string font_name =        //haven't chosen a font yet..
+	const string font_name = "arial.ttf";   
 
+	//create the window
     int pixelWidth = VideoMode::getDesktopMode().width / 2;
     int pixelHeight = VideoMode::getDesktopMode().height / 2;
     VideoMode vm(pixelWidth, pixelHeight);
     RenderWindow window(vm, "Mandelbrot", Style::Default);
 
+	//create the plane
+	ComplexPlane plane(pixelWidth, pixelHeight);
+
+	//text and font
     Font font;
     if (!font.loadFromFile(font_name))
     {
@@ -17,7 +22,7 @@ int main()
 
     Text text;
     text.setFont(font);
-    text.setCharacterSize(100);
+    text.setCharacterSize(25);
     text.setFillColor(sf::Color::White);
 
     while (window.isOpen())
@@ -35,20 +40,46 @@ int main()
 			if (event.type == sf::Event::Closed) window.close();
 			if (event.type == sf::Event::MouseButtonPressed)
 			{
-				if (event.mouseButton.button == sf::Mouse::Left)
+				if (event.mouseButton.button == sf::Mouse::Left)			//handle left click to zoom in and center the window
 				{
-					std::cout << "the left button was pressed" << std::endl;
-					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-
-					update = true;
+					plane.zoomIn();
+					plane.setCenter(Vector2i(event.mouseButton.x, event.mouseButton.y));
 				}
+				if (event.mouseButton.button == sf::Mouse::Right)			//handle right click to zoom out and center the window
+				{
+					plane.zoomOut();
+					plane.setCenter(Vector2i(event.mouseButton.x, event.mouseButton.y));
+				}
+			}
+			if (event.type == sf::Event::MouseMoved)				//update cursor location 
+			{
+				plane.setMouseLocation(Vector2i(event.mouseButton.x, event.mouseButton.y));
 			}
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
 		}
+
+		/*
+		****************************************
+		Update
+		****************************************
+		*/
+
+		plane.updateRender();		//update the scene
+		plane.loadText(text);
+
+		/*
+		****************************************
+		Draw the scene
+		****************************************
+		*/
+
+		window.clear();					//draw and display
+		window.draw(plane);
+		window.draw(text);
+		window.display();
 
     }
 }
